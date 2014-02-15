5e86010b0becdb96c33babd530cf1f43261b5848
kubo0180 <gibson02122569@gmail.com>
2014-02-15 10:52:13 +0900

    add until data method

--- transcriptionQuery.js
+++ transcriptionQuery.js
@@ -17,7 +17,7 @@ var _jQuery = window.jQuery,
 
 var jQuery = window.jQuery = window.$ = function( selector, context ) {
   // The jQuery object is actually just the init constructor 'enhanced'
-  return new jQUery.fn.init( selector, context );
+  return new jQuery.fn.init( selector, context );
 };
 
 // A simple way to check for HTML strings or ID strings
@@ -351,5 +351,130 @@ jQuery.fn = jQuery.prototype = {
 
   is: function( selector ) {
     return !!selector && jQuery.multiFilter( selector, this ).length > 0;
+  }, 
+
+  hasClass: function( selector ) {
+    return this.is( "." + selector );
+  }, 
+
+  val: function( value ) {
+    if ( value == undefined ) {
+
+      if ( this.length ) {
+        var elem = this[0];
+
+        // We need to handle select boxes special
+        if ( jQuery.nodeName( elem, "select" ) ) {
+          var index = elem.selectedIndex, 
+            values = [], 
+            options = elem.options, 
+            one = elem.type == "select-one";
+
+          // Nothing was selected 
+          if ( index < 0 )
+            return null;
+
+          // Loop through all the selected options
+          for ( var i = one ? index : 0, max = one ? index + 1 : options.length; i < max; i++ ) {
+            var option = options[ i ];
+
+            if ( option.selected ) {
+              // Get the specifc value for the option
+              value = jQuery.browser.msie && !option.attributes.value.specified ? option.text : option.value;
+
+              // We don't need an array for one selects
+              if ( one )
+                return value;
+
+              // Multi-Selects return an array
+              values.push( value );
+            }
+          }
+
+          return values;
+
+        // Everything else, we just grab the value
+        } else 
+          return (this[0].value || "").replace(/\r/g, "");
+
+      }
+
+      return undefined;
+    }
+
+    if ( value.constructor == Number )
+      value += '';
+
+    return this.each(function(){
+      if ( this.nodeType != 1 )
+        return;
+
+      if ( value.constructor == Array && /radio|checkbox/.test( this.type ) )
+        this.checked = (jQuery.inArray(this.value, value) >= 0 ||
+          jQuery.inArray(this.name, value) >= 0);
+
+      else if ( jQuery.nodeName( this, "select" ) ) {
+        var values = jQuery.makeArray(value);
+
+        jQuery( "option", this ).each(function(){
+          this.selected = (jQuery.inArray( this.value, values ) >= 0 ||
+            jQuery.inArray( this.text, values ) >= 0);
+        });
+
+        if ( !values.length )
+          this.selectedIndex = -1;
+
+      } else
+        this.value = value;
+    });
+  }, 
+
+  html: function( value ) {
+    return value == undefined ? 
+      (this[0] ? 
+       this[0].innerHTML :
+       null) :
+      this.empty().append( value );
+  }, 
+
+  replaceWith: function( value ) {
+    return this.after( value ).remove();
+  }, 
+
+  eq: function( i ) {
+    return this.slice( i, i + 1 );
+  }, 
+
+  slice: function() {
+    return this.pushStack( Array.prototype.slice.apply( this, arguments ) );
+  }, 
+
+  map: function( callback ) {
+    return this.pushStack( jQuery.map(this, function(elem, i){
+      return callback.call( elem, i, elem );
+    }));
+  }, 
+
+  andSelf: function() {
+    return this.add( this.prevObject );
+  }, 
+
+  data: function( key, value ) {
+    var parts = key.split(".");
+    parts[1] = parts[1] ? "." + parts[1] : "";
+
+    if ( value === undefined ) {
+      var data = this.triggerHandler("getData" + parts[i] + "!", [parts[0]]);
+
+      if ( data === undefined && this.length )
+        data = jQuery.data( this[0], key );
+      
+      return data === undefined && parts[1] ? 
+        this.data( parts[0] ) : 
+        data;
+    } else 
+      return this.trigger("setData" + parts[1] + "!", [parts[0], value]).each(function(){
+        jQuery.data( this, key, value );
+      });
   }, 
 
